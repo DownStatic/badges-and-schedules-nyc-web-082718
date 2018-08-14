fffd6a19e506dfbf4e6b05a4f91b943e0bb93367
Christopher Stull <chris.stull@gmail.com>
2018-08-14 18:33:16 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,7 +1,7 @@
 # Write your code here.
 
 def badge_maker(name)
-  return "Hello, my name is #{name}"
+  return "Hello, my name is #{name}."
 end
 
 def batch_badge_creator(arr)
@@ -15,7 +15,7 @@ end
 
 def assign_rooms(speakers)
   assignments = []
-  for i in (0..6)
+  for i in (0..(speakers.size-1))
     assignments.push("Hello, #{speakers[i]}, You'll be assigned to room #{(i+1)}!")
   end
   return assignments
