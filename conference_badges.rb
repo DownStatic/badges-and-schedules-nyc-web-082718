f04ffc04359b834338adba53d29d4d5914f1113c
Christopher Stull <chris.stull@gmail.com>
2018-08-14 18:21:00 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,15 @@
 # Write your code here.
+
+def badge_maker(name)
+  return "Hello, my name is #{name}"
+end
+
+def batch_badge_creator(arr)
+  name_arr=[]
+  count=0
+  arr.each do |name|
+    name_arr[count]=badge_maker(name)
+    count = count + 1
+  end
+end
+
